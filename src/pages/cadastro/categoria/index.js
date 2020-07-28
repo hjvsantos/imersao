6cf2ff67f7c8229205bef8142cf12474fcad8902
hjvsantos <joao__lucas_santos@hotmail.com>
2020-07-28 19:56:41 -0300

    Ajuste no Slider

--- src/pages/cadastro/categoria/index.js
+++ src/pages/cadastro/categoria/index.js
@@ -6,9 +6,18 @@ function CadastroCategoria(){
     return(
       <PageDefault>
         <h1>Cadastro de Categoria</h1>
+          <form>
+            <label>
+              Nome do artista:
+              <input
+                type = "text"
+              />
+            </label>
 
-
-        
+            <button>
+              Cadastrar 
+            </button>          
+          </form>        
         <Link to="/">
             Home Page
         </Link>
