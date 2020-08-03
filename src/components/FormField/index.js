b90aaa096b857bc44ae6eef8e39b4568b0f80b1b
hjvsantos <joao__lucas_santos@hotmail.com>
2020-08-02 23:09:15 -0300

    Inicio da estruturacao do beck e utilizaçao do ESL

--- src/components/FormField/index.js
+++ src/components/FormField/index.js
@@ -1,19 +1,115 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable react/prop-types */
 import React from 'react';
+import PropTypes from 'prop-types';
+import styled, { css } from 'styled-components';
 
-function FormField({titulo, type, name, value, onChange}){
-return(
-    <div>
-        <label>
-            {titulo} 
-            <input
-            type = {type}
-            name = {name}
-            value={value}
-            onChange = {onChange}
-              />
-        </label>
-        </div>
-)
+const FormFieldWrapper = styled.div`
+  position: relative;
+  textarea{
+    min-height: 150px;
+  }
+`;
+
+const Label = styled.label`
+
+`;
+Label.Text = styled.span`
+  color: darkgreen;
+  height: 57px;
+  position: absolute;
+  top: 0;
+  left: 16px;
+
+  display: flex;
+  align-items: center;
+
+  transform-origin: 0% 0%;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 300;
+
+  transition: .1s ease-in-out;
+`;
+
+const Input = styled.input`
+  background: #C6F0A2;
+  color: darkgreen;
+  display: block;
+  width: 100%;
+  height: 57px;
+  font-size: 18px;
+
+  outline:0;
+  border: 0;
+  border-top: 4px solid transparent;
+  border-bottom: 4px solid transparent;
+  margin-bottom: 10px;
+
+  padding: 16px 16px;
+  border-radius: 4px;
+  transition: border-color .3s;
+
+  &:focus{
+    border-bottom-color:(--primary);
+  }
+
+  &:focus + span{
+    transform: scale(.6) translateY(-10px);
+  }
+
+  ${({ hasValeu }) => hasValeu && css`
+      &:not([type="color"]) + span{
+      transform: scale(.6) translateY(-10px);  
+      }
+    `}
+`;
+
+function FormField({
+  titulo,
+  type,
+  name,
+  value,
+  onChange,
+}) {
+  const fieldId = `id_${name}`;
+  const isTextArea = type === 'textarea';
+  const tag = isTextArea ? 'textarea' : 'input';
+
+
+  return (
+    <FormFieldWrapper>
+      <Label
+        htmlFor={fieldId}
+      >
+        <Input
+          as={tag}
+          id={fieldId}
+          type={type}
+          name={name}
+          value={value}
+          onChange={onChange}
+        />
+        <Label.Text>
+          {titulo}
+        </Label.Text>
+      </Label>
+    </FormFieldWrapper>
+  );
 }
+
+FormField.defaultProps = {
+  type: 'text',
+  value: '',
+  onChange: () => {},
+};
+
+FormField.propTypes = {
+  titulo: PropTypes.string.isRequired,
+  type: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+};
 
 export default FormField;
--- src/pages/cadastro/categoria/index.js
+++ src/pages/cadastro/categoria/index.js
@@ -1,82 +1,85 @@
-import React, {useState} from 'react';
-import PageDefault from '../../../components/PageDefault';
+/* eslint-disable react/button-has-type */
+/* eslint-disable linebreak-style */
+// eslint-disable-next-line import/no-named-as-default-member
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import PageDefault from '../../../components/PageDefault';
 import FormField from '../../../components/FormField';
+import Button from '../../../components/Button';
 
-function CadastroCategoria(){
-  const valorInicial={
-    nome:'',
-    descricao:''
-  }
+function CadastroCategoria() {
+  const valorInicial = {
+    nome: '',
+    descricao: '',
+  };
   const [categorias, setCategorias] = useState([]);
   const [values, setValues] = useState(valorInicial);
 
-
-
-  function setValue(key, value){
+  function setValue(key, value) {
     setValues({
-      ...values, 
+      ...values,
       [key]: value,
-    })
+    });
   }
 
-  function handleChange(e){
+  function handleChange(e) {
     setValue(
       e.target.getAttribute('name'),
-      e.target.value
-      )                
+      e.target.value,
+    );
   }
 
-    return(
-      <PageDefault>
-        <h1>Novo Artista:  {values.nome}</h1>
-          <form onSubmit={function handleSubmit(e){
-            e.preventDefault();
-            setCategorias([
-              ...categorias,
-              values,
-              setValues(valorInicial)
-            ]);
-            
-          }}>
-            {/* State */}
-            
-            <FormField 
-              titulo = 'Novo Artista:'
-              type = {Text}
-              name = 'nome'
-              value = {values.nome}
-              onChange = {handleChange}
-            />
+  return (
+    <PageDefault>
+      <h1>
+        Novo Artista:
+        {values.nome}
+      </h1>
+      <form onSubmit={function handleSubmit(e) {
+        e.preventDefault();
+        setCategorias([
+          ...categorias,
+          values,
+          setValues(valorInicial),
+        ]);
+      }}
+      >
+        {/* State */}
 
-            <FormField 
-              titulo = 'Descrição:'
-              type = {Text}
-              name = 'descricao'
-              value = {values.descricao}
-              onChange = {handleChange}
-            />
+        <FormField
+          titulo="Novo Artista:"
+          type={Text}
+          name="nome"
+          value={values.nome}
+          onChange={handleChange}
+        />
 
-            <button>
-              Cadastrar 
-            </button>          
-          </form>    
+        <FormField
+          titulo="Descrição:"
+          type="textarea"
+          name="descricao"
+          value={values.descricao}
+          onChange={handleChange}
+        />
 
-          <ul>
-            {categorias.map((categoria, indice) => {
-              return(
-                <li key={`${categoria}${indice}`}>
-                  {categoria.nome}
-                </li>
-              )
-            })}
-          </ul>
+        <Button>
+          Cadastrar
+        </Button>
+      </form>
 
-        <Link to="/">
-            Home Page
-        </Link>
-      </PageDefault>
-    )
-  }
+      <ul>
+        {categorias.map((categoria) => (
+          <li key={`${categoria.nome}`}>
+            {categoria.nome}
+          </li>
+        ))}
+      </ul>
 
-  export default CadastroCategoria;
+      <Link to="/">
+        Home Page
+      </Link>
+    </PageDefault>
+  );
+}
+
+export default CadastroCategoria;
